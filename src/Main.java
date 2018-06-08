efb023dff43d2aaca3837db41d26d14937711e2b
Denis <dikin.deni@gmail.com>
2018-06-08 22:09:57 +0300

    Добавлен новый HW

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Another HW");
-        System.out.println("3 HW");
     }
 }
